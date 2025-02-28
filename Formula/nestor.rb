ad8fc9ee50322791e6def973f79489a537234517
Aurélien Rainone <476650+arl@users.noreply.github.com>
2025-02-28 12:17:06 +0100

    Update nestor.rb

--- Formula/nestor.rb
+++ Formula/nestor.rb
@@ -2,14 +2,15 @@
 class Nestor < Formula
   desc "NES emulator"
   homepage "https://github.com/arl/nestor"
-  url "https://github.com/arl/nestor/archive/v0.1.0.tar.gz"
-  sha256 "80781802f95fa6d4a9532ae9b3de254f7d69a6af935cf2acc19587508b66d857"
+  url "https://github.com/arl/nestor/releases/download/untagged-c03687dbf69fe1485195/nestor-macos.tar.gz"
+  sha256 "273af3c351330af98f022510d4ccf850b73ed4939979a3252b512c597ac98da3"
   license "GPLv3"
-  version "0.1.0"
+  version "0.0.1"
 
   depends_on "go@1.24" => :build 
   depends_on "gtk+3"
   depends_on "sdl2"
+  depends_on "sdl2_ttf"  
 
   def install
     go_bin = Formula["go"].opt_bin/"go"
