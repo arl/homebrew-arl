74059bd55e7f4cc71cea59573a66f687f2e37562
Aurélien Rainone <aurelien.rainone@gmail.com>
2025-03-26 18:17:42 +0100

    Formula: update nestor v0.0.1

--- Formula/nestor.rb
+++ Formula/nestor.rb
@@ -1,26 +1,30 @@
-
 class Nestor < Formula
-  desc "NES emulator"
+  desc "NES emulator."
   homepage "https://github.com/arl/nestor"
-  url "https://github.com/arl/nestor/releases/download/untagged-c03687dbf69fe1485195/nestor-macos.tar.gz"
-  sha256 "273af3c351330af98f022510d4ccf850b73ed4939979a3252b512c597ac98da3"
-  license "GPLv3"
   version "0.0.1"
+  license "GPLv3"
 
-  depends_on "go@1.24" => :build 
+  depends_on "go@1.24"
   depends_on "gtk+3"
   depends_on "sdl2"
   depends_on "sdl2_ttf"  
 
-  def install
-    go_bin = Formula["go"].opt_bin/"go"
-    version_output = Utils.safe_popen_read(go_bin, "version")
-    required_version = "go1.24"
-    unless version_output.include?(required_version)
-      odie "Nestor requires Go #{required_version} or later. Installed version: #{version_output}"
-    end
+  on_macos do
+    on_intel do
+      url "https://github.com/arl/nestor/releases/download/v0.0.1/nestor-macos-x64.tgz"
+      sha256 "9fbf1fc928c6a2d216b3ce5ad23f7768d0550477e18b845c6996cae426aadd4b"
 
-    ENV["CGO_ENABLED"] = "1"
-    system go_bin, "build", *std_go_args(ldflags: "-s -w"), "./cmd/nestor"
+      def install
+        bin.install "nestor"
+      end
+    end
+    on_arm do
+      url "https://github.com/arl/nestor/releases/download/v0.0.1/nestor-macos-arm64.tgz"
+      sha256 "412a33581b93aa23295fcae857c5fc128a03ee267b16d002ff9a194817a823b8"
+
+      def install
+        bin.install "nestor"
+      end
+    end
   end
 end
